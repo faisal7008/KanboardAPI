1515544e6dfc6f14398c447ec82976f19ce2a932
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2024-02-21 02:00:41 +0530

    Established connection to Redis Cloud

--- redisClient.js
+++ redisClient.js
@@ -1,15 +1,25 @@
-const Redis = require('ioredis');
+require('dotenv').config()
+const Redis = require("ioredis");
 
 // Create a Redis client instance
 const redisClient = new Redis({
-  host: 'localhost', // Redis server host
-  port: 6379, // Redis server port
-  // Add more options as needed (e.g., password, TLS configuration)
+  // host: 'localhost', 
+  // port: 6379,
+  username: process.env.REDIS_CLOUD_USERNAME,
+  password: process.env.REDIS_CLOUD_PASSWORD,
+  host: process.env.REDIS_CLOUD_HOST,
+  port: process.env.REDIS_CLOUD_PORT,
 });
 
 // Handle connection errors
-redisClient.on('error', (err) => {
-  console.error('Redis error:', err);
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
 });
+
+// Handle connection errors
+redisClient.on("connect", () => {
+  console.error("Redis server is connected...");
+});
+
 
 module.exports = redisClient;
